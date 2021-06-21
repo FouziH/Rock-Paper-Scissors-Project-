f134d773566ef4cb604651fb3d2602232a1c6f37
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-21 12:29:33 -0500

    Creating a function that takes an input and return that input as long as they are equal to rock, paper, or scissors. If the input is not equal to those preselected inputs, it will return and error message.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,3 +13,32 @@
 // Get the computer’s choice.
 // Compare the two choices and determine a winner.
 // Start the program and display the results.
+
+//1. The user should be able to choose ‘rock’, ‘paper’, or ‘scissors’ when the game starts.
+
+//2. Using const and arrow function syntax, create a function named getUserChoice that takes a single parameter userInput.
+//Since a user can pass in a parameter, such as ‘Rock’ or ‘rock’ with different capitalizations, begin by utilizing JavaScript’s toLowerCase() function to make the userInput all lowercase.
+//you can use code like this userInput = userInput.toLowerCase();
+
+
+//3.When getting the user’s choice, you should also check to make sure that the user typed a valid choice: ‘rock’, ‘paper’, or ‘scissors’.
+//Inside getUserChoice(), write an if/else statement that makes sure the userInput is either 'rock', 'paper', or 'scissors'.
+//If it does, then return the userInput.
+//If not, use console.log to print an error message to the console.
+const getUserChoice = userInput => {
+userInput = userInput.toLowerCase();
+if(userInput === "rock" || userInput === "paper" || userInput === "scissors"){
+  return userInput;
+}else {
+  console.log("There was an error. Please ensure you are inputs are one of the following, rock, paper, or scissors. ");
+}
+};
+//5/Now we need to have the computer make a choice.
+
+//Create a new function named getComputerChoice with no parameters.
+//Inside its block, utilize Math.random() and Math.floor() to get a whole number between 0 and 2.
+//Then, depending on the number, return either 'rock', 'paper', or 'scissors'.
+
+const getComputerChoice =()=>{
+  const randomNum = Math.floor(Math.random() * 2);
+}
